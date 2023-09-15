70b7ffe640501bd88ea15666c2e71d2ec18baa10
Jérôme Dautreme <jerome.dautreme@live.fr>
2023-09-15 10:58:22 +0200

    selection perso, tableaux associatifs

--- site/personnage.php
+++ site/personnage.php
@@ -16,26 +16,58 @@
     </header>
     <main>
         <form action="#" method="get">
-            <label for="sexe"> Choix du personnage:</label>
-            <select name="sexe" id="sexe">
-                <option value="Femme">Femme</option>
-                <option value="Homme">Homme</option>
+            <label for="profil"> Choix du personnage:</label>
+            <select name="profil" id="profil" onchange="submit()">
+                <option value="Katy" <?php if(isset($_GET["profil"])&& $_GET["profil"] === "Katy")echo "selected" ?>>Katy</option>
+                <option value="Robert" <?php if(isset($_GET["profil"])&& $_GET["profil"]==="Robert")echo "selected" ?>>Robert</option>
+                <option value="Yves" <?php if(isset($_GET["profil"])&& $_GET["profil"]==="Yves")echo "selected" ?>>Yves</option>
             </select>
-            <input type="submit" value="Envoyer">
         </form>
-        <!-- <img src="./assets/images/playerF.png" alt="dessin femme"> -->
+        <h2>Personnage :</h2>
         <?php
-        if (isset($_GET["sexe"])) {
-            if ($_GET["sexe"]==="Femme") {
-                echo '<img src="./assets/images/playerF.png" alt="dessin femme">';
-            }else {
-                echo '<img src="./assets/images/player.png" alt="dessin homme">';
-            
-            }
-        } else {
-            echo "<h3>Veuillez envoyer votre choix dans le champs ci-dessus</h3>";
-        }
+        $personnages = [
+            "Katy" => [
+                "Nom : "     => "Katy",
+                "Age : "     => 22,
+                "Sexe : "    => "Femme",
+                "Force : "   => 3,
+                "Agilité : " => 6
+            ],
+            "Robert" => [
+                "Nom : "     => "Robert",
+                "Age : "     => 28,
+                "Sexe : "    => "Homme",
+                "Force : "   => 5,
+                "Agilité : " => 4
+            ],
+            "Yves" => [
+                "Nom : "     => "Yves",
+                "Age : "     => 22,
+                "Sexe : "    => "Homme",
+                "Force : "   => 2,
+                "Agilité : " => 7
+            ]
+        ];
         
+        function afficherPersonnage($personnage) {
+            echo '<figure style="display: flex; align-items: end;margin-left: 10px;">';
+            echo '<img src="./assets/images/player' . $personnage["Nom : "] . '.png" alt="dessin ' . $personnage["Nom : "] . '">';
+            echo "<figcaption style='margin-left: 10px'>";
+            foreach ($personnage as $cle => $valeur) {
+                echo "$cle$valeur<br>";
+            }
+            echo "</figcaption>";
+            echo "</figure>";
+        }
+
+        if (isset($_GET["profil"])) {
+            $profil = $_GET["profil"];
+            if (isset($personnages[$profil])) {
+                afficherPersonnage($personnages[$profil]);
+            }
+        }
+
+                
         ?>
     </main>
 </body>
